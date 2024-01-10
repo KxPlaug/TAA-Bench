c87528587c406939ce082700ee92e47e0e3f711c
KxPlaug <3175389813@qq.com>
2024-01-10 09:51:55 +0800

    update

--- attacks/igattack.py
+++ attacks/igattack.py
@@ -4,10 +4,7 @@ import scipy.stats as st
 import torch.nn.functional as F
 from attacks.attack import Attack
 import numpy as np
-import copy
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
 from captum.attr import IntegratedGradients
 import torch
 import torch.nn as nn
@@ -29,29 +26,6 @@ def compute_integrated_gradient(batch_x, batch_blank, model, idx):
     return integrated_gradients
 
 class IGAttack(Attack):
-    r"""
-    BIM or iterative-FGSM in the paper 'Adversarial Examples in the Physical World'
-    [https://arxiv.org/abs/1607.02533]
-
-    Distance Measure : Linf
-
-    Arguments:
-        model (nn.Module): model to attack.
-        eps (float): maximum perturbation. (Default: 8/255)
-        alpha (float): step size. (Default: 2/255)
-        steps (int): number of steps. (Default: 10)
-
-    .. note:: If steps set to 0, steps will be automatically decided following the paper.
-
-    Shape:
-        - images: :math:`(N, C, H, W)` where `N = number of batches`, `C = number of channels`,        `H = height` and `W = width`. It must have a range [0, 1].
-        - labels: :math:`(N)` where each value :math:`y_i` is :math:`0 \leq y_i \leq` `number of labels`.
-        - output: :math:`(N, C, H, W)`.
-
-    Examples::
-        >>> attack = torchattacks.BIM(model, eps=8/255, alpha=2/255, steps=10)
-        >>> adv_images = attack(images, labels)
-    """
     def __init__(self, model, eps=8/255, alpha=2/255, steps=10,decay=1.0):
         super().__init__("IGAttack", model)
         self.eps = eps
@@ -61,11 +35,10 @@ class IGAttack(Attack):
         else:
             self.steps = steps
         self.supported_mode = ['default', 'targeted']
-        self.ig = IntegratedGradients(model)
         self.model = model
         self.mu = decay
 
-    def forward(self, images, labels, save_func,save_steps,output_dir):
+    def forward(self, images, labels, save_func,output_dir):
         r"""
         Overridden.
         """
@@ -73,33 +46,19 @@ class IGAttack(Attack):
 
         images = images.clone().detach().to(self.device)
         labels = labels.clone().detach().to(self.device)
-        all_zero_predict = self.model(torch.zeros_like(images))
-        all_zero_loss = nn.CrossEntropyLoss()(all_zero_predict, labels).item()
-        all_one_predict = self.model(torch.ones_like(images))
-        all_one_loss = nn.CrossEntropyLoss()(all_one_predict, labels).item()
-        original_predict = self.model(images)
-        original_loss = nn.CrossEntropyLoss()(original_predict, labels).item()
         adv_images = images.clone().detach()
         last_g = torch.zeros_like(images).detach().to(self.device)
         for iter in range(self.steps):
-            delta_t = -self.ig.attribute(adv_images, baselines=torch.zeros_like(adv_images), target=labels).float()
-            # delta_t = agi(self.model,adv_images, labels)
-            # all_zero_delta_t = -compute_integrated_gradient(adv_images, torch.zeros_like(adv_images), self.model, labels)
-            # all_one_delta_t = -compute_integrated_gradient(adv_images, torch.ones_like(adv_images), self.model, labels) / abs(original_loss - all_one_loss)
-            # delta_t = all_zero_delta_t + all_one_delta_t
-            # delta_t = all_zero_delta_t
-            # delta_t = mfaba_smooth(self.model,adv_images,labels)
-            # delta_t = torch.from_numpy(delta_t).float().to(self.device)
-            # g = self.mu * last_g + delta_t / torch.norm(delta_t, p=1)
+            # delta_t = -self.ig.attribute(adv_images, baselines=torch.zeros_like(adv_images), target=labels).float()
+            delta_t = -compute_integrated_gradient(adv_images, torch.zeros_like(adv_images), self.model, labels)
             g = self.mu * last_g + delta_t / (delta_t).abs().mean(dim=[1,2,3],keepdim=True)
             last_g = g
             adv_images = adv_images.detach() + self.alpha * g.sign()
             adv_images = torch.max(torch.min(adv_images, images + self.eps), images - self.eps).detach()
             adv_images = torch.clamp(adv_images, 0.0, 1.0).detach()
-            if iter == self.steps-1:
-                # print((model(x).argmax(dim=-1) == labels).float().mean())
-                adv_img_np = adv_images.detach().cpu().numpy()
-                adv_img_np = np.transpose(adv_img_np, (0, 2, 3, 1)) * 255
-                save_func(images=adv_img_np,output_dir=output_dir[:-1]+f"_{iter}/")
+
+        adv_img_np = adv_images.detach().cpu().numpy()
+        adv_img_np = np.transpose(adv_img_np, (0, 2, 3, 1)) * 255
+        save_func(images=adv_img_np,output_dir=output_dir[:-1]+f"_{iter}/")
         return adv_images
     
